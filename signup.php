60494c0885766b368f15356a2a738b7cb8234170
gio <jaiyo.contact@gmail.com>
2018-06-04 14:03:43 +0200

    ok

--- signup.php
+++ signup.php
@@ -7,10 +7,26 @@
 	<link rel="icon" href="sign-check-icon.png">
 </head>
 <body>
+
 	<form method="post" action="signup_post.php">
-		<label for="mail">Adresse email : </label><br/><input type="text" name="mail" id="mail"><br/><br/>
-		<label for="mdp1">Mot de passe : </label> <br/><input type="password" name="mdp1" id="mdp1"><br/><br/>
-		<label for="mdp2">Confirmez votre mot de passe : </label> <br/><input type="password" name="mdp2" id="mdp2"><br/>
+
+		<label for="mail">Adresse mail : </label><br/><input type="email" name="mail" id="mail" required="required" pattern='^[a-z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$' ><br/><br/>
+
+		<label for="mdp1">Mot de passe : </label> <br/><input type="password" name="mdp1" id="mdp1" required="required"><br/><br/>
+
+		<label for="mdp2">Confirmez votre mot de passe : </label> <br/><input type="password" name="mdp2" id="mdp2" required="required"><br/>
+
+		<?php
+			if($_POST['mdp1'] == $_POST['mdp2'])
+			{
+			   // Enregistre
+			}
+			else
+			{
+			   echo 'mots de passes pas identiques';
+			}
+		?>
+
 		<input type="submit" name="envoyer">
 		
 	</form>
--- signup_post.php
+++ signup_post.php
@@ -1,19 +1,25 @@
 <?php 
 
+//connexion a la BDD
 try{
-$bdd=new PDO ('mysql:host=localhost;dbname=jaiyo;charset=utf8','root','');
+$bdd=new PDO ('mysql:host=localhost;dbname=jaiyo;charset=utf8','root','root');
 }
 catch (Exception $e){
 	die('Erreur : '. $e->getMessage());
 }
 
+//test pour les mdp
 if (isset($_POST['mdp1']) AND isset($_POST['mdp2'])) {
-	echo "2 set \n";
-	if ($_POST['mdp1']==$_POST['mdp2']) {
+
+	if (($_POST['mdp1']==$_POST['mdp2']) AND strlen($_POST['mdp1'])>4) {
 		echo "ok";
+		header('Location: accueil.php');
 	}
 	else{
 		echo "pas ok";
+		$_POST['vmdp']='false';
+
+		header('Location: signup.php');
 	}
 }
 
